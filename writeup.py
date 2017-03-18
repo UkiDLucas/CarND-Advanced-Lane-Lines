b03c00f72955a85868551ad6dfc544c2c965f8b7
Uki D. Lucas <UkiDLucas@gmail.com>
2017-03-17 21:42:41 -0500

    started on writeup

---
+++ writeup.py
@@ -0,0 +1,14 @@
+
+# coding: utf-8
+
+# # Udacity project 4 - Andvanced Road Lane Lines
+# 
+# This project is suppoeded to meet following requirements:
+# https://review.udacity.com/#!/rubrics/571/view
+# 
+# 
+
+# In[ ]:
+
+
+
